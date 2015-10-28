14a91991f0394b87f32b346985242bc68ce7d0a2
Haldun Bayhantopcu <hb@haldun.me>
2015-10-28 05:51:15 +0100

    Initial Commit

---
+++ TinderLike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderLike
+//
+//  Created by Haldun Bayhantopcu on 28/10/15.
+//  Copyright © 2015 monoid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
